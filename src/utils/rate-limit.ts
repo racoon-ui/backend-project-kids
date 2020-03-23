d9d3871d1a7ab65a0e92395b006d9d2a5ed4c3be
wecanooo <wecanooo@gmail.com>
2020-03-23 15:11:18 +0900

    change RateLimit store from redis to memory

--- src/utils/rate-limit.ts
+++ src/utils/rate-limit.ts
@@ -1,14 +1,18 @@
 import rateLimit from 'express-rate-limit';
 import RedisStore from 'rate-limit-redis';
-import Redis from 'ioredis';
+// import Redis from 'ioredis';
 import constants from '@config/constants';
 
-const client = new Redis(constants.REDIS_PORT, constants.REDIS_HOST);
+// const client = new Redis(constants.REDIS_PORT, constants.REDIS_HOST);
 
 const limiter = rateLimit({
-  store: new RedisStore({
-    client,
-  }),
+  /**
+   * 기본설정인 memory store 를 사용합니다.
+   * 만약, Redis Store 를 사용하려면 아래의 구문을 주석해제 합니다.
+   */
+  // store: new RedisStore({
+  //   client,
+  // }),
   windowMs: 1 * 60 * 1000,
   max: 100,
 });
